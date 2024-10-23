8e5a7634e0bf4b6935afbf436bf8fbc1c6e28ee3
Anderson Vargas Sepulveda <andersonvargas383@gmail.com>
2024-10-22 22:34:26 -0500

    fix ajuste

--- app/(student)/payment/index.tsx
+++ app/(student)/payment/index.tsx
@@ -8,47 +8,49 @@ export default function Payment() {
   const fetchDataPayment = async () => {
     try {
       const { data } = await Axios.post(`/payment/create-payment`);
+      setWompiHTML(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Wompi Payment</title>
+        </head>
+        <body>
+            <form>
+              <script
+                src="https://checkout.co.uat.wompi.dev/widget.js"
+                data-render="button"
+                data-public-key="pub_stagtest_g2u0HQd3ZMh05hsSgTS2lUV8t3s4mOt7"
+                data-currency="COP"
+                data-amount-in-cents="${data.totalPayment}"	
+                data-reference="${data.reference}"
+                data-signature:integrity="${data.signature}">
+              </script>
+            </form>
+        </body>
+        </html>
+      `);
 
-      // setWompiHTML(`
-      //   <!DOCTYPE html>
-      //   <html lang="en">
-      //   <head>
-      //       <meta charset="UTF-8">
-      //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      //       <title>Wompi Payment</title>
-      //   </head>
-      //   <body>
-      //       <form>
-      //         <script
-      //           src="https://checkout.wompi.co/widget.js"
-      //           data-render="button"
-      //           data-public-key="pub_stagtest_g2u0HQd3ZMh05hsSgTS2lUV8t3s4mOt7"
-      //           data-currency="COP"
-      //           data-amount-in-cents="1500000"
-      //           data-reference="${data.reference}"
-      //           data-signature:integrity="${data.signature}">
-      //         </script>
-      //       </form>
-      //   </body>
-      //   </html>
-      // `);
     } catch (error) {
       console.log('🚀 ~ handleDelete ~ error', error);
     }
   }
+
+  console.log(wompiHTML);
 
   useEffect(() => {
     fetchDataPayment();
   },[])
 
   return (
-    wompiHTML && <WebView
+    wompiHTML ? <WebView
       originWhitelist={['*']}
       source={{ html: wompiHTML }}
       style={{ flex: 1 }}
       javaScriptEnabled={true}
       domStorageEnabled={true}
-    />
+    /> : <View style={styles.container} ></View>
   );
 }
 
